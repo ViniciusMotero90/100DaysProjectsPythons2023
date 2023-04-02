a5a797126e9ca7b04fc4a723edf7efb2fa3da989
ViniciusMotero90 <48352985+ViniciusMotero90@users.noreply.github.com>
2023-04-01 22:27:03 -0300

    12/100 days projects Pythons 2023

---
+++ Day12/main.py
@@ -0,0 +1,22 @@
+enemies = 1
+
+#def increase_enemies():
+#    enemies = 2
+#    print(f"enemies inside function: {enemies}")
+
+#increase_enemies()
+#print(f"enemies outside function: {enemies}")
+
+#Local Scope
+#def drink_potoin():
+#    potoin_strength = 2
+#    print(potoin_strength)
+
+#drink_potoin()
+
+#Global Scope
+player_health = 10
+
+def drink_potoin():
+    potoin_strength = 2
+    print(player_health)
