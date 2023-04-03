ebaa287f51887af40b805a80365ccf09b0e40190
ViniciusMotero90 <48352985+ViniciusMotero90@users.noreply.github.com>
2023-04-03 14:18:55 -0300

    17/100 days projects Pythons 2023

---
+++ Day17/quiz_game_start/data.py
@@ -0,0 +1,56 @@
+question_data = [
+    {
+        "category": "Entertainment: Video Games",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "English new wave musician Gary Numan founded the video game development company Facepunch Studios in March 2009.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Entertainment: Video Games",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "The Mann Co. Store from Team Fortress 2 has the slogan &quot;We hire mercenaries and get in fights&quot;.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Entertainment: Video Games",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "In Kingdom Hearts the Paopu fruit is said to intertwine the destinies for two people forever.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+     },
+    {
+        "category": "Entertainment: Video Games",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "The 2005 video game &quot;Call of Duty 2: Big Red One&quot; is not available on PC.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Entertainment: Video Games",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "In Team Fortress 2, being disguised as a scout or medic results in a speed boost.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+     },
+    {"category": "Entertainment: Video Games", "type": "boolean", "difficulty": "easy",
+     "question": "In Splatoon, the Squid Sisters are named Tako and Yaki.", "correct_answer": "False",
+     "incorrect_answers": ["True"]},
+    {"category": "Entertainment: Video Games", "type": "boolean", "difficulty": "easy",
+                                      "question": "In the game &quot;Subnautica&quot;, a &quot;Spadefish&quot; will attack you.",
+                                      "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "Entertainment: Video Games", "type": "boolean", "difficulty": "easy",
+     "question": "In Pok&eacute;mon Sun and Moon, a male Salandit can evolve to a Salazzle.", "correct_answer": "False",
+     "incorrect_answers": ["True"]},
+    {"category": "Entertainment: Video Games", "type": "boolean", "difficulty": "easy",
+                                      "question": "In Terraria, you can craft the Cell Phone pre-hardmode.",
+                                      "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"category": "Entertainment: Video Games", "type": "boolean", "difficulty": "easy",
+     "question": "In the &quot;S.T.A.L.K.E.R.&quot; series, the Freedom faction wishes to destroy the supernatural area known as  &quot;the Zone&quot;.",
+     "correct_answer": "False", "incorrect_answers": ["True"]}]
