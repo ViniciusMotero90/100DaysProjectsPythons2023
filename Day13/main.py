732cc1bd1536f6d7756e2694e81fd2b5b4e874e5
ViniciusMotero90 <48352985+ViniciusMotero90@users.noreply.github.com>
2023-04-02 22:02:05 -0300

    15/100 days projects Pythons 2023

---
+++ Day13/main.py
@@ -0,0 +1,17 @@
+#def my_function():
+#    for i in range(1, 20): #0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19
+#        if i == 20: #Nunca vai chega ao 20, pois esse range começa no 0 termina no 19 não no 20
+#            print("You got it")
+#def my_function():
+#    for i in range(1, 21):
+#        if i == 20:
+#            print("You got it")
+#my_function()
+
+#year = int(input("What's your year of birth?"))
+#if year > 1980 and year < 1994:
+#    print("You're a millenial.")
+#elif year >= 1994:
+#    print("You're a Gen Z.")
+
+
