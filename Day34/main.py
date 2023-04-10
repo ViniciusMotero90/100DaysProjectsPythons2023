f4b2419619865c32dde7f4a28594920c1cf6fb33
ViniciusMotero90 <48352985+ViniciusMotero90@users.noreply.github.com>
2023-04-10 17:29:15 -0300

    34/100 days projects Pythons 2023

---
+++ Day34/main.py
@@ -0,0 +1,16 @@
+#age: int
+#name: str
+#height: float
+#is_human: bool
+
+def police_check(age: int) -> bool:
+    if age > 18:
+        can_drive = True
+    else:
+        can_drive = False
+    return "They can drive"
+
+if police_check("twelve"):
+    print("You may pass")
+else:
+    print("Pay a fine.")
